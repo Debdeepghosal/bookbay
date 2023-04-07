dcfb734b53c5f5160517e59aa365f62e3f9b2c82
Debdeepghosal <Debdeepghosal@github.com>
2023-04-07 14:54:58 +0530

    added sonarqube file

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.145.8.248:9000/',
+  serverUrl: 'http://35.175.179.60:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
